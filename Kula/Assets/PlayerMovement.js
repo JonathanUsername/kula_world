ad0f7a25c8c4ba78b7397fbe2e4d274d3260505a
Jonathan King <jonathan.j.king@gmail.com>
2016-08-22 19:36:25 +0100

    Started scene

--- Kula/Assets/PlayerMovement.js
+++ Kula/Assets/PlayerMovement.js
@@ -1,21 +1,73 @@
 ﻿#pragma strict
 
+var speed : float = 0.1;
+var rotationSpeed : float = 1000.0;
+var jumpSpeed : float = 1.0;
+var gravity : float = 1.0;
+
+private var moveDirection : Vector3 = Vector3.zero;
+private var rolling : boolean = false;
+
+
+while(true) yield CoUpdate();
+
 function Start () {
 
 }
 
+function CoUpdate() {
+
+  var controller : CharacterController = GetComponent.<CharacterController>();
+
+  var jumpVec;
+
+  if (controller.isGrounded) {
+    if (Input.GetButton ('Jump')) {
+      jumpVec = Vector3(0, jumpSpeed, 0);
+    }
+
+    if (Input.GetKeyDown(KeyCode.RightArrow))
+      yield Rotate(Quaternion.Euler(0, 90, 0));
+    else if (Input.GetKeyDown(KeyCode.LeftArrow))
+      yield Rotate(Quaternion.Euler(0, -90, 0));
+    else if (Input.GetKeyDown(KeyCode.UpArrow) && Input.GetButton('Jump'))
+      yield Move(this.transform.forward + Vector3(0, 1, 0));
+    else if (Input.GetKeyDown(KeyCode.UpArrow))
+      yield Move(this.transform.forward);
+    else if (Input.GetKeyDown(KeyCode.DownArrow))
+      yield Move(this.transform.forward * -1);
+    else 
+      yield;
+
+  }
+
+  // Apply gravity
+  moveDirection.y -= gravity * Time.deltaTime;
+  
+  // Move the controller
+  controller.Move(moveDirection * Time.deltaTime);
+
+}
+
+function Move(distance : Vector3) {
+  var pt = this.transform;
+  var goal = pt.position + distance;
+  while (pt.position != goal) {
+    pt.position = Vector3.MoveTowards(pt.position, goal, speed * Time.deltaTime);
+    yield;
+  }
+}
+
+function Rotate(rotation : Quaternion) {
+  var pt = this.transform;
+  var goal = pt.rotation * rotation;
+  while (pt.rotation != goal) {
+    pt.rotation = Quaternion.RotateTowards(pt.rotation, goal, 10);
+    yield;
+  }
+}
+
 function Update () {
-  if (Input.GetKeyDown ('up')) {
-    this.transform.position += this.transform.forward * 1;
-  }
-  if (Input.GetKeyDown ('down')) {
-    this.transform.position -= this.transform.forward * 1;
-  }
-  if (Input.GetKeyDown ('left')) {
-    this.transform.rotation *= Quaternion.Euler(0, 90, 0);
-  }
-  if (Input.GetKeyDown ('right')) {
-    this.transform.rotation *= Quaternion.Euler(0, -90, 0);
-  }
+
   
 }
--- Kula/Assets/TrackingCamera.js
+++ Kula/Assets/TrackingCamera.js
@@ -7,6 +7,7 @@ function Start () {
 function Update () {
   var h = 2;
   var player = GameObject.Find('Ball');
+  // print(player.transform.position);
   var cameraHeight = new Vector3(0, 1, 0);
   var cameraBackwardsOffset = player.transform.forward * 5;
   this.transform.position = player.transform.position + cameraHeight - cameraBackwardsOffset;
