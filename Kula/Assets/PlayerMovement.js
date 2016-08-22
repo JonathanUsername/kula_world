fb79e88c69fea03547b374e6949a6115ba91d032
Jonathan King <jonathan.j.king@gmail.com>
2016-08-22 17:39:38 +0100

    Movement basics working

---
+++ Kula/Assets/PlayerMovement.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+  if (Input.GetKeyDown ('up')) {
+    this.transform.position += this.transform.forward * 1;
+  }
+  if (Input.GetKeyDown ('down')) {
+    this.transform.position -= this.transform.forward * 1;
+  }
+  if (Input.GetKeyDown ('left')) {
+    this.transform.rotation *= Quaternion.Euler(0, 90, 0);
+  }
+  if (Input.GetKeyDown ('right')) {
+    this.transform.rotation *= Quaternion.Euler(0, -90, 0);
+  }
+  
+}
---
+++ Kula/Assets/TrackingCamera.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+  var h = 2;
+  var player = GameObject.Find('Ball');
+  this.transform.position = player.transform.position - player.transform.forward * 5;
+  this.transform.LookAt(player.transform);
+}
